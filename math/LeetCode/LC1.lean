3b7adffae0de02815dc5a0d915a9c3f05c91136d
tdzl2003 <tdzl2003@gmail.com>
2025-03-17 20:45:00 +0800

    -

--- math/LeetCode/LC1.lean
+++ math/LeetCode/LC1.lean
@@ -8,6 +8,8 @@ https://leetcode.cn/problems/two-sum/description/
 import Mathlib.Tactic
 import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
+open Classical
+
 -- 给定的参数
 variable (nums: Array Nat)
 variable (target: Nat)
@@ -20,10 +22,6 @@ def IsAnswer(ans: Nat × Nat) :=
   else
     False
 
-structure AnswerWithProof where
-  ans: Nat × Nat
-  proof: IsAnswer nums target ans
-
 -- 已知存在答案
 -- 题目中提到的已知只存在一个有效答案只影响判定，不影响解的正确性
 def HasAnswer := ∃ ans, IsAnswer nums target ans
@@ -33,7 +31,7 @@ def Solution1: Nat × Nat := Id.run do
   for a in Array.finRange nums.size do
     for b in Array.finRange nums.size do
       if a<b ∧ nums[a]+nums[b] = target then
-        return (a, b)
+        return (a.val, b.val)
   return (0,0)
 
 -- 样例数据
@@ -41,28 +39,47 @@ def Solution1: Nat × Nat := Id.run do
 #eval Solution1 [3,2,4].toArray 6
 #eval Solution1 [3,3].toArray 6
 
--- 带有证明逻辑的Solution1
-def Solution1_with_maybe_proof{β}(gen: (ans: Nat×Nat) -> (proof: IsAnswer nums target ans) -> β): β := Id.run do
+-- 解法1的正确性证明
+theorem solution1_is_right(hHasAnswer: HasAnswer nums target) : IsAnswer nums target <| Solution1 nums target := by
+  conv =>
+    arg 3
+    simp [Solution1, Id.run]
+  split
+  {
+    -- 无解分支，通过和有解推出矛盾证明
+    rename_i x heq
+    sorry
+  }
+  {
+    -- 有解分支，证明解的正确性
+    rename_i x ans heq
+    sorry
+  }
+
+structure AnswerWithProof where
+  ans: Nat × Nat
+  proof: HasAnswer nums target -> IsAnswer nums target ans
+
+
+-- 带有证明逻辑的Solution1，很难证明其和Solution1的等价性，但可以直接使用它得到一个带证明的实现
+def Solution1_1_with_proof: AnswerWithProof nums target := Id.run do
   for a in Array.finRange nums.size do
     for b in Array.finRange nums.size do
       if h: a<b ∧ nums[a.val]+nums[b.val] = target then
-        return gen (a.val, b.val) (by
+        return AnswerWithProof.mk (a.val, b.val) (by
           {
+            intro hHasAnswer
             simp [IsAnswer]
             simp [h.left]
             have x: a.val < nums.size∧b.val < nums.size := ⟨a.isLt, b.isLt⟩
             exact h.right
           })
-  return gen (0,0) (by sorry)
+  -- 无解情形的证明：需证明和有解矛盾，这个证明仍然涉及对for的拆解，比较困难
+  return AnswerWithProof.mk (0,0) (by sorry)
 
--- 解法1的正确性证明
-theorem solution1_is_answer(hHasAnswer: HasAnswer nums target) : IsAnswer nums target <| Solution1 nums target := by
-  sorry
-
-
+def Solution1_1: ℕ×ℕ :=
+    (Solution1_1_with_proof nums target).ans
 
 
-
-
--- 解法1的复杂度证明
--- TODO
+theorem Solution1_1_is_right(hHasAnswer: HasAnswer nums target) : IsAnswer nums target <| Solution1_1 nums target := by
+  exact (Solution1_1_with_proof nums target).proof hHasAnswer
---
+++ math/LeetCode/Merge.lean
@@ -0,0 +1,18 @@
+-- 参考 https://github.com/Qiu233/MergeSort/blob/master/MergeSort/Basic.lean
+-- 尝试以另一种结构给出相同的实现和证明
+
+import Mathlib.Data.List.Perm
+import Mathlib.Tactic.Linarith
+
+variable {α : Type}
+
+variable [LinearOrder α]
+
+def Merge : List α → List α → List α
+  | [], y => y
+  | x, [] => x
+  | a :: x, b :: y =>
+    if a ≤ b
+      then a :: Merge x (b :: y)
+      else b :: Merge (a :: x) y
+termination_by x y => x.length + y.length
